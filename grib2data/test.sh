cef9d61e795ee101376a9537b9cc941ef7fab764
Erwan Cossevin <erwan.cossevin@meteo.fr>
2025-06-17 09:42:08 +0000

    Move test output to the build directory, for grib2data

--- grib2data/test.sh
+++ grib2data/test.sh
@@ -14,10 +14,15 @@ f=$1
 LFITOOLS=$2
 p=$3
 grib_api_prefix=$4
+build_dir=$5
 
 f_name=$(echo "$f" | sed 's/^[^/]*\///; s/\//_/g')
 
-cd $p
+cd $build_dir
+cd tests_results
+test_name=$(basename $p)
+cd $test_name
+
 
 export GRIB_DEFINITION_PATH=$p/extra_grib_defs:$grib_api_prefix/share/definitions:$grib_api_prefix/share/eccodes/definitions
 export GRIB_SAMPLES_PATH=$grib_api_prefix/ifs_samples/grib1:$grib_api_prefix/share/eccodes/ifs_samples/grib1
@@ -35,27 +40,25 @@ print "@list"
 
 
 
-\rm -f "test/$f"
-d=$(dirname "test/$f")
-mkdir -p $d
+\rm -f "$f"
 
-$LFITOOLS fatestgrib2data --fa-file-1 $f  --fa-file-2 "test/$f"  
-$LFITOOLS lfidiff --lfi-file-1 $f  --lfi-file-2 "test/$f" --out "test/$f.diff"
+$LFITOOLS fatestgrib2data --fa-file-1 $p/$f  --fa-file-2 $f_name
+$LFITOOLS lfidiff --lfi-file-1 $p/$f  --lfi-file-2 "$f_name" --out "$f_name.diff"
 
 \rm -f zero.$f_name.grib pack.$f_name.grib
 
-$LFITOOLS extractgrib --fa-file $f  --grib-file zero.$f_name.grib --only $(file2list test/$f.diff)
-$LFITOOLS extractgrib --fa-file "test/$f" --grib-file pack.$f_name.grib --only "file://test/$f.diff"
+$LFITOOLS extractgrib --fa-file $p/$f  --grib-file zero.$f_name.grib --only $(file2list $f_name.diff)
+$LFITOOLS extractgrib --fa-file "$f_name" --grib-file pack.$f_name.grib --only "file://$f_name.diff"
 
 ls -l zero.$f_name.grib pack.$f_name.grib
 
-if [ -s zero.grib.$f ]
+if [ -s zero.$f_name.grib ]
 then
 
 $grib_api_prefix/../bin/grib_dump -O zero.$f_name.grib > zero.$f_name.txt
 $grib_api_prefix/../bin/grib_dump -O pack.$f_name.grib > pack.$f_name.txt
 
-\rm -f zero.grib.$f pack.grib.$f
+\rm -f zero.$f_name.grib pack.$f_name.grib
 
 perl -i -ne ' print unless (m/^\s*\**\s+FILE:/o) ' zero.$f_name.txt pack.$f_name.txt
 
